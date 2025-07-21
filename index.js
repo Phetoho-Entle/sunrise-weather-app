36109c2d558ac23f57bdf43ab90661e3b7f09833
Phetoho-Entle <phetomohohla1122@gmail.com>
2025-07-21 13:26:44 +0200

    Added dynamic weather info

---
+++ index.js
@@ -0,0 +1,49 @@
+function search (event) {
+event.preventDefault();
+let searchInputElement = document.querySelector("#search-input");
+let city = searchInputElement.value;
+
+let apiKey = "739f904148cfa8boc86t8a8b426aac3f";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = `${temperature}<span id="degrees">°</span>`;
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+
+    let iconElement = document.querySelector("#weather-icon-img");
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt",response.data.condition.description);
+
+  let weatherConditionsElement = document.querySelector("#weather-conditions");
+  weatherConditionsElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+
+
+
+
+}
+
+function loadDefaultCity () {
+    let city = "London";
+    let apiKey = "739f904148cfa8boc86t8a8b426aac3f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+
+}
+
+loadDefaultCity();
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit",search);
